d6d828d121f2cf40ca9730c29f2978fe3234cf56
John Rey Juele <15167798@studentmail.ul.ie>
2017-03-16 14:17:12 +0000

    Fixed registration problem
    This works in the CSIS server

--- Website Appearance (Sophia)/connect.php
+++ Website Appearance (Sophia)/connect.php
@@ -1,7 +1,9 @@
 <?php
-
+$servername = "localhost";
+$username = "group4";
+$pass = "various-also-material-ears";
 try {
-    $dbh = new PDO("mysql:host=localhost;dbname=web project", "root", "");
+    $dbh = new PDO("mysql:host=testweb3;dbname=group4", "group4", "various-also-material-ears");
 } catch (PDOException $exception)  {
     printf("Connection error: %s", $exception->getMessage());
 }
--- Website Appearance (Sophia)/login.php
+++ Website Appearance (Sophia)/login.php
@@ -2,33 +2,23 @@
 
 if (isset($_POST['id']) && isset($_POST['pass'])) {
 
-    require("checkUserExistence.php");
-    if (checkUserExistence($_POST['id'], $_POST['pass']) == true) {
-        
-    	require("connect.php");
-    	$result = $dbh->prepare("Select * from users where User_ID = ".$_POST['id']);
-        $result->execute();
-        echo "<table>";
-        echo "<tr><th>User_ID</th><th>FirstName</th><th>LastName</th><th>Email</th><th>Subject</th><th>Reputation Points</th></tr>";
-        for ($i = 0; $row = $result->fetch(); $i++) {
-        	// TODO get user's homepage from Sophia
-        	// Have sessions implemented
-        	// Use Object Oriented Approach
-            echo "<tr><td>".$row['User_ID']."</td><td>".$row['FirstName']."</td><td>".$row['LastName']."</td><td>".$row['Email']."</td><td>".$row['Subject']."</td><td>".$row['Rep_Points']."</td></tr>";
-    	}
-    	echo "</table>";
-    $dbh = null;
-
-
-    } else {
-        echo "<h1>ID with entered password do not exist</h1>";
-        echo "<a href='index.html'>Click here to register</a>";
-         echo "<a href='AlternativeLogin.html'>Click here to login again</a>";
-    }
+	require("checkUserExistence.php");
+	if (checkUserExistence($_POST['id'], $_POST['pass']) == true) {
+		  
+		session_start();
+		$_SESSION['user_id'] = $_POST['id'];
+		header("Location: home_page.php");
+		
+		
+	} else {
+		echo "<h1>ID with entered password do not exist</h1>";
+		echo "<a href='index.html'>Click here to register</a>";
+	   echo "<a href='AlternativeLogin.html'>Click here to login again</a>";
+	}
 
 } else {
-    echo "<h1>Form was not entered properly</h1>";
-    echo "<a href='AlternativeLogin.html'>Click here to go back</a>";
+   echo "<h1>Form was not entered properly</h1>";
+   echo "<a href='AlternativeLogin.html'>Click here to go back</a>";
 }
 
 
