59960d8edc60261a4347cc69650db7f5626de2a1
John Rey Juele <15167798@studentmail.ul.ie>
2017-04-23 22:47:41 +0100

    Updated values file

--- Documents/Database Schemas/values.sql
+++ Documents/Database Schemas/values.sql
@@ -1,9 +1,23 @@
 INSERT INTO Users (User_ID, FirstName, LastName, Email, Subject, Password) VALUES
-(11111111, 'John', 'Juele', '11111111@studentmail.ul.ie', 111, 'John1234'),
-(22222222, 'Sophia', 'Colgan', '22222222@studentmail.ul.ie', 111, 'Sophia1234'),
-(33333333, 'Eoghan', 'Casey', '33333333@studentmail.ul.ie', 111, 'Eoghan1234'),
-(44444444, 'Donagh', 'Kelleher', '444444443@studentmail.ul.ie', 111, 'Donagh1234'),
-(55555555, 'John', 'Doe', '55555555@studentmail.ul.ie', 222, 'John1234'),
-(66666666, 'Bob', 'Marley', '66666666@studentmail.ul.ie', 222, 'Bob1234');
+(11111111, 'John', 'Juele', '11111111@studentmail.ul.ie', 111, 'John1234#'),
+(22222222, 'Sophia', 'Colgan', '22222222@studentmail.ul.ie', 111, 'Sophia1234#'),
+(33333333, 'Eoghan', 'Casey', '33333333@studentmail.ul.ie', 111, 'Eoghan1234#'),
+(44444444, 'Donagh', 'Kelleher', '444444443@studentmail.ul.ie', 111, 'Donagh1234#'),
+(55555555, 'John', 'Doe', '55555555@studentmail.ul.ie', 222, 'John1234#'),
+(66666666, 'Bob', 'Marley', '66666666@studentmail.ul.ie', 222, 'Bob1234#'),
+(77777777, 'UserOne', 'OneUser', '77777777@studentmail.ul.ie', 222, 'Bob1234#'),
+(88888888, 'UserTwo', 'UserTwo', '88888888@studentmail.ul.ie', 222, 'Bob1234#'),
+(99999999, 'UserThree', 'UserThree', '99999999@studentmail.ul.ie', 222, 'Bob1234#'),
+(10101010, 'UserFour', 'UserFour', '10101010@studentmail.ul.ie', 222, 'Bob1234#'),
+(00000001, 'UserFive', 'UserFive', '00000001@studentmail.ul.ie', 222, 'Bob1234#'),
+(00000010, 'UserSix', 'UserSix', '00000010@studentmail.ul.ie', 222, 'Bob1234#'),
+(00000011, 'UserSeven', 'UserSeven', '00000011@studentmail.ul.ie', 222, 'Bob1234#'),
+(00000100, 'UserEight', 'UserEight', '00000100@studentmail.ul.ie', 222, 'Bob1234#'),
+(00000101, 'UserNine', 'UserNine', '00000101@studentmail.ul.ie', 222, 'Bob1234#'),
+(00000110, 'UserTen', 'UserTen', '00000110@studentmail.ul.ie', 222, 'Bob1234#'),
+(00000111, 'UserEleven', 'UserEleven', '00000111@studentmail.ul.ie', 222, 'Bob1234#'),
+(00001000, 'UserTwelve', 'UserTwelve', '00001000@studentmail.ul.ie', 222, 'Bob1234#'),
+(00001001, 'UserThirteen', 'UserThirteen', '00001001@studentmail.ul.ie', 222, 'Bob1234#'),
+(00001010, 'UserFourteen', 'UserFourteen', '00001010@studentmail.ul.ie', 222, 'Bob1234#');
 
 
